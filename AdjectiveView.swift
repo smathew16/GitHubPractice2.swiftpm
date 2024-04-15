42e5a64eeb49b006ba7daf2c5fba1e45cfa286e1
Angel Bitsov <143033010+TheArchangelWarrior@users.noreply.github.com>
2024-04-15 14:38:47 -0500

    Added AdjectiveView

---
+++ AdjectiveView.swift
@@ -0,0 +1,18 @@
+//
+//  AdjectiveView.swift
+//  GitHubPractice2
+//
+//  Created by Angel Bitsov on 4/15/24.
+//
+
+import SwiftUI
+
+struct AdjectiveView: View {
+    var body: some View {
+        NavigationLink("Summary", SummaryView())
+    }
+}
+
+#Preview {
+    AdjectiveView()
+}
