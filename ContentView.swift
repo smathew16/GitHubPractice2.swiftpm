2b25ecd6565f2e434c31772c22acc81759cb58d2
Santo <smathew7247@stu.d214.org>
2024-04-15 14:26:34 -0500

    Got rid of extra things

--- ContentView.swift
+++ ContentView.swift
@@ -3,13 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView{
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                NavigationLink("NounView", destination: NounView())
-            }
+            
+            NavigationLink("NounView", destination: NounView())
         }
     }
 }
+
