ec1a7e07d27754befd074c236d72a7b40ae54b93
Santo <smathew7247@stu.d214.org>
2024-04-15 14:56:52 -0500

    Connected madlib and made madlib

--- NounView.swift
+++ NounView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct NounView: View {
+    
+    @State var Madlib: MadLib = MadLib(noun: "", adjective: "", verb: "")
+    
     var body: some View {
-        NavigationLink("VerbView", destination: VerbView())
+        NavigationLink("VerbView", destination: VerbView(madlib: Madlib))
+        
+        TextField("Noun Here", text: $Madlib.noun)
+        
     }
 }
 
