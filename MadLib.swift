c25a59f3a881bcf45eb47d36d85a580926850e64
Angel Bitsov <143033010+TheArchangelWarrior@users.noreply.github.com>
2024-04-15 14:49:10 -0500

    Added MadLib struct qith noun, adj., and verb properties.

---
+++ MadLib.swift
@@ -0,0 +1,14 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Angel Bitsov on 4/15/24.
+//
+
+import SwiftUI
+
+struct MadLib{
+    var noun: String
+    var adjective: String
+    var verb: String
+}
